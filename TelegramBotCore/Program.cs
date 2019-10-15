796fa5168a0cda8c09eb304d267a376b11ad77f9
AskoldMakaruk <askoldmakaruk@gmail.com>
2019-10-15 11:43:47 +0300

    why even its here

--- TelegramBotCore/Program.cs
+++ TelegramBotCore/Program.cs
@@ -6,21 +6,8 @@ namespace TelegramBotCore
 {
     internal class Program
     {
-        abstract class One
-        {
-            public static string IsIt => "Yeah";
-        }
-
-        abstract class Twho : One
-        {
-            public new static string IsIt => "Nope";
-        }
-
         private static void Main(string[] args)
         {
-            Console.WriteLine(One.IsIt);
-            Console.WriteLine(Twho.IsIt);
-
 
             var bot = new Client("{YOUR TOKEN HERE}");
             while (true)
