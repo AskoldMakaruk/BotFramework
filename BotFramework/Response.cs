9083c95854a0c29ced31007563228cf86c830d6a
AskoldMakaruk <askoldmakaruk@gmail.com>
2019-12-21 14:19:55 +0200

    refactored response

--- BotFramework/Response.cs
+++ BotFramework/Response.cs
@@ -11,63 +11,65 @@ namespace BotFramework
 {
     public class Response
     {
-        public Response(ICommand command)
+        public Response(ICommand command) : this()
         {
             NextPossible = new Either<ICommand, IEnumerable<IOneOfMany>>(command);
-            Responses = ImmutableList<ResponseMessage>.Empty;
         }
 
-        public Response(params IOneOfMany[] nextPossible)
+        public Response(params IOneOfMany[] nextPossible) : this()
         {
-            if(nextPossible.Length > 0)
+            if (nextPossible.Length > 0)
                 NextPossible = new Either<ICommand, IEnumerable<IOneOfMany>>(nextPossible).ToOptional();
-            Responses = ImmutableList<ResponseMessage>.Empty;
         }
 
-        public Response(IEnumerable<IOneOfMany> nextPossible)
+        public Response(IEnumerable<IOneOfMany> nextPossible) : this()
         {
             NextPossible = new Either<ICommand, IEnumerable<IOneOfMany>>(nextPossible);
+        }
+
+        public Response()
+        {
             Responses = ImmutableList<ResponseMessage>.Empty;
         }
 
         private Response(Response old, ImmutableList<ResponseMessage> newResponses)
         {
-           Responses = newResponses;
-           NextPossible = old.NextPossible;
+            Responses    = newResponses;
+            NextPossible = old.NextPossible;
         }
 
-        public readonly ImmutableList<ResponseMessage> Responses; 
+        public readonly ImmutableList<ResponseMessage> Responses;
 
         public readonly Optional<Either<ICommand, IEnumerable<IOneOfMany>>> NextPossible;
 
-        #region Constructors
+#region Constructors
 
-        public Response SendTextMessage(ChatId chat, string text, IReplyMarkup replyMarkup = null,
-            int replyToMessageId = 0, ParseMode parseMode = default)
+        public Response SendTextMessage(ChatId chat,                 string    text, IReplyMarkup replyMarkup = null,
+                                        int    replyToMessageId = 0, ParseMode parseMode = default)
         {
             var toAdd = new ResponseMessage(ResponseType.TextMessage)
             {
-            ChatId = chat,
-            Text = text,
-            ReplyMarkup = replyMarkup,
-            ReplyToMessageId = replyToMessageId,
-            ParseMode = parseMode
+                ChatId           = chat,
+                Text             = text,
+                ReplyMarkup      = replyMarkup,
+                ReplyToMessageId = replyToMessageId,
+                ParseMode        = parseMode
             };
-            return new Response(this, this.Responses.Add(toAdd));
+            return new Response(this, Responses.Add(toAdd));
         }
 
-        public Response EditTextMessage(ChatId chatId, int editMessageId, string text,
-            IReplyMarkup replyMarkup = null, ParseMode parseMode = default)
+        public Response EditTextMessage(ChatId       chatId,             int       editMessageId, string text,
+                                        IReplyMarkup replyMarkup = null, ParseMode parseMode = default)
         {
             var toAdd = new ResponseMessage(ResponseType.EditTextMesage)
             {
-            ChatId = chatId,
-            EditMessageId = editMessageId,
-            Text = text,
-            ReplyMarkup = replyMarkup,
-            ParseMode = parseMode
+                ChatId        = chatId,
+                EditMessageId = editMessageId,
+                Text          = text,
+                ReplyMarkup   = replyMarkup,
+                ParseMode     = parseMode
             };
-            return new Response(this, this.Responses.Add(toAdd));
+            return new Response(this, Responses.Add(toAdd));
         }
 
         public Response AnswerQueryMessage(string callbackQueryId, string text)
@@ -75,60 +77,60 @@ namespace BotFramework
             var toAdd = new ResponseMessage(ResponseType.AnswerQuery)
             {
                 AnswerToMessageId = callbackQueryId,
-                Text = text
+                Text              = text
             };
-            return new Response(this, this.Responses.Add(toAdd));
+            return new Response(this, Responses.Add(toAdd));
         }
 
-        public Response SendDocument(long account,
-            InputOnlineFile document,
-            string caption = null,
-            int replyToMessageId = 0,
-            IReplyMarkup replyMarkup = null)
+        public Response SendDocument(long            account,
+                                     InputOnlineFile document,
+                                     string          caption          = null,
+                                     int             replyToMessageId = 0,
+                                     IReplyMarkup    replyMarkup      = null)
         {
             var toAdd = new ResponseMessage(ResponseType.SendDocument)
             {
-            ChatId = account,
-            Text = caption,
-            ReplyToMessageId = replyToMessageId,
-            ReplyMarkup = replyMarkup,
-            Document = document
+                ChatId           = account,
+                Text             = caption,
+                ReplyToMessageId = replyToMessageId,
+                ReplyMarkup      = replyMarkup,
+                Document         = document
             };
-            return new Response(this, this.Responses.Add(toAdd));
+            return new Response(this, Responses.Add(toAdd));
         }
 
-        public Response EditMessageMarkup(ChatId accountChatId, int messageMessageId,
-            InlineKeyboardMarkup addMemeButton)
+        public Response EditMessageMarkup(ChatId               accountChatId, int messageMessageId,
+                                          InlineKeyboardMarkup addMemeButton)
         {
             var toAdd = new ResponseMessage(ResponseType.EditMessageMarkup) {ChatId = accountChatId, MessageId = messageMessageId, ReplyMarkup = addMemeButton};
-            return new Response(this, this.Responses.Add(toAdd));
+            return new Response(this, Responses.Add(toAdd));
         }
 
-        public Response SendPhoto(ChatId accountChatId, InputOnlineFile document, string caption = null,
-            int replyToMessageId = 0, IReplyMarkup replyMarkup = null)
+        public Response SendPhoto(ChatId accountChatId,        InputOnlineFile document, string caption = null,
+                                  int    replyToMessageId = 0, IReplyMarkup    replyMarkup = null)
         {
             var toAdd = new ResponseMessage(ResponseType.SendPhoto)
             {
-            ChatId = accountChatId,
-            Text = caption,
-            ReplyToMessageId = replyToMessageId,
-            ReplyMarkup = replyMarkup,
-            Document = document
+                ChatId           = accountChatId,
+                Text             = caption,
+                ReplyToMessageId = replyToMessageId,
+                ReplyMarkup      = replyMarkup,
+                Document         = document
             };
-            return new Response(this, this.Responses.Add(toAdd));
+            return new Response(this, Responses.Add(toAdd));
         }
 
         public Response SendSticker(ChatId accountChatId, InputOnlineFile sticker)
         {
             var toAdd = new ResponseMessage(ResponseType.Sticker)
             {
-                ChatId = accountChatId,
+                ChatId   = accountChatId,
                 Document = sticker
             };
-            return new Response(this, this.Responses.Add(toAdd));
+            return new Response(this, Responses.Add(toAdd));
         }
 
-        #endregion
+#endregion
     }
 
     public class ResponseMessage
