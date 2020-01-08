a1bded1511c58681729a4c15ba9515632daf9a51
AskoldMakaruk <askoldmakaruk@gmail.com>
2020-01-08 12:53:52 +0200

    Added echobot example based on BF v0.2.1

---
+++ BotFramework.Examples/EchoBot/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using BotFramework.Bot;
+using BotFramework.Commands;
+using BotFramework.Responses;
+using Telegram.Bot.Types;
+
+namespace EchoBot
+{
+    class Program
+    {
+        static void Main()
+        {
+            new BotBuilder()
+            .UseAssembly(typeof(Program).Assembly)
+            .WithName("EchoBot")
+            .WithToken("<YOUR TOKEN>")
+            .UseLogger((c, m) => Console.WriteLine(m))
+            .Build()
+            .Run();
+        }
+    }
+
+    public class EchoCommand : MessageCommand
+    {
+        public override Response Execute(Message message, Client client)
+        {
+            return new Response().AddMessage(new TextMessage(message.Chat.Id, message.Text));
+        }
+
+        public override bool Suitable(Message message) => true; //accept any message
+    }
+}
