6530587d1c345e7f1a5cf4a41afa4370287b3236
Askold Makaruk <askoldmakaruk@gmail.com>
2020-02-05 10:58:50 +0200

    Added new responses

---
+++ BotFramework/Responses/CreateNewStickerSet.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.InputFiles;
+
+namespace BotFramework.Responses
+{
+    public class CreateNewStickerSet : IResponseMessage
+    {
+        public CreateNewStickerSet(int             userId,
+                                   string          name,
+                                   string          title,
+                                   InputOnlineFile pngSticker,
+                                   string          emojis,
+                                   bool            isMasks      = false,
+                                   MaskPosition    maskPosition = null)
+        {
+            UserId       = userId;
+            Name         = name;
+            Title        = title;
+            PngSticker   = pngSticker;
+            Emojis       = emojis;
+            IsMasks      = isMasks;
+            MaskPosition = maskPosition;
+        }
+
+        public int             UserId       { get; }
+        public string          Name         { get; }
+        public string          Title        { get; }
+        public InputOnlineFile PngSticker   { get; }
+        public string          Emojis       { get; }
+        public bool            IsMasks      { get; }
+        public MaskPosition    MaskPosition { get; }
+
+        public Task Send(TelegramBotClient botClient)
+        {
+            return botClient.CreateNewStickerSetAsync(UserId, Name, Title, PngSticker, Emojis, IsMasks, MaskPosition);
+        }
+    }
+}
---
+++ BotFramework/Responses/SetStickerPositionInSet.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace BotFramework.Responses
+{
+    public class SetStickerPositionInSet : IResponseMessage
+    {
+        public SetStickerPositionInSet(string sticker,
+                                       int    position)
+        {
+            Sticker  = sticker;
+            Position = position;
+        }
+
+        public string Sticker  { get; }
+        public int    Position { get; }
+
+        public Task Send(TelegramBotClient botClient)
+        {
+            return botClient.SetStickerPositionInSetAsync(Sticker, Position);
+        }
+    }
+}
