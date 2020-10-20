28bf298e14a124ef8e2d384c51f2b18de646133d
BananchickPasha <smidy999green@gmail.com>
2020-10-20 20:28:14 +0300

    thread safe queue

--- BotFramework/Clients/Client.cs
+++ BotFramework/Clients/Client.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -12,27 +13,27 @@ namespace BotFramework.Clients
 {
     public class Client : IClient
     {
-        private TelegramBotClient  _client;
-        public  BasicBotTask?      CurrentBasicBotTask;
-        public  BotTask<Response>? CurrentTask;
-        public  Queue<Update>      UpdatesToHandle = new Queue<Update>();
+        private TelegramBotClient                   _client;
+        public  BasicBotTask?                       CurrentBasicBotTask;
+        public  BotTask<Response>?                  CurrentTask;
+        public  IProducerConsumerCollection<Update> UpdatesToHandle = new ConcurrentQueue<Update>();
         public Client(TelegramBotClient client, long userId) => (_client, UserId) = (client, userId);
 
         public BasicBotTask GetUpdateAsync(Func<Update, bool>? filter = null)
         {
             CurrentBasicBotTask = new BasicBotTask(filter);
-            while (!CurrentBasicBotTask.IsCompleted && UpdatesToHandle.Count > 0)
-                CurrentBasicBotTask.HandleUpdate(UpdatesToHandle.Dequeue());
+            while (!CurrentBasicBotTask.IsCompleted && UpdatesToHandle.TryTake(out var update))
+                CurrentBasicBotTask.HandleUpdate(update);
             return CurrentBasicBotTask;
         }
 
         public void HandleUpdate(Update update)
         {
-            UpdatesToHandle.Enqueue(update);
+            UpdatesToHandle.TryAdd(update);
             if (CurrentBasicBotTask == null || CurrentBasicBotTask.IsCompleted)
                 return;
-            while (!CurrentBasicBotTask.IsCompleted && UpdatesToHandle.Count > 0)
-                CurrentBasicBotTask.HandleUpdate(UpdatesToHandle.Dequeue());
+            while (!CurrentBasicBotTask.IsCompleted && UpdatesToHandle.TryTake(out update))
+                CurrentBasicBotTask.HandleUpdate(update);
         }
 
         public Task<TResponse> MakeRequestAsync<TResponse>(IRequest<TResponse> request,
