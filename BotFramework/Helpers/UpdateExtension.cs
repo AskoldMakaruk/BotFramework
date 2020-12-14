78fd1e8bad4a773e076494f42e6d53014e6a3881
Askold Makaruk <askoldmakaruk@gmail.com>
2020-12-14 20:07:26 +0200

    0.5.0-dev-1

--- BotFramework/Helpers/UpdateExtension.cs
+++ BotFramework/Helpers/UpdateExtension.cs
@@ -6,44 +6,44 @@ namespace BotFramework.Helpers
 {
     public static class UpdateExtensions
     {
-        public static long GetFromId(this Update update)
+        public static User? GetUser(this Update update)
         {
-            long from;
+            User from;
             switch (update.Type)
             {
                 case UpdateType.Message:
                     var message = update.Message;
-                    from = message.From.Id;
+                    from = message.From;
                     break;
                 case UpdateType.InlineQuery:
-                    from = update.InlineQuery.From.Id;
+                    from = update.InlineQuery.From;
                     break;
                 case UpdateType.ChosenInlineResult:
-                    from = update.ChosenInlineResult.From.Id;
+                    from = update.ChosenInlineResult.From;
                     break;
                 case UpdateType.CallbackQuery:
-                    from = update.CallbackQuery.From.Id;
+                    from = update.CallbackQuery.From;
                     break;
                 case UpdateType.EditedMessage:
-                    from = update.EditedMessage.From.Id;
+                    from = update.EditedMessage.From;
                     break;
                 case UpdateType.ChannelPost:
-                    from = update.ChannelPost.From.Id;
+                    from = update.ChannelPost.From;
                     break;
                 case UpdateType.EditedChannelPost:
-                    from = update.EditedChannelPost.From.Id;
+                    from = update.EditedChannelPost.From;
                     break;
                 case UpdateType.ShippingQuery:
-                    from = update.ShippingQuery.From.Id;
+                    from = update.ShippingQuery.From;
                     break;
                 case UpdateType.PreCheckoutQuery:
-                    from = update.PreCheckoutQuery.From.Id;
+                    from = update.PreCheckoutQuery.From;
                     break;
                 case UpdateType.Poll:
                 case UpdateType.PollAnswer: 
                 case UpdateType.Unknown:
                 default:
-                    return default;
+                    return null;
             }
 
 
