c57610c57e91929efb26d8ddc97794c80e9aff01
Askold Makaruk <askoldmakaruk@gmail.com>
2021-06-30 15:18:29 +0300

    Basics

---
+++ BotFramework/Middleware/LoggingMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using BotFramework.Abstractions;
+using BotFramework.Helpers;
+using Serilog;
+using Serilog.Context;
+using Telegram.Bot.Types;
+
+namespace BotFramework.Middleware
+{
+    public class LoggingMiddleware
+    {
+        private readonly UpdateDelegate _next;
+        private readonly ILogger        _logger;
+
+        public LoggingMiddleware(UpdateDelegate next, ILogger logger)
+        {
+            _next   = next;
+            _logger = logger;
+        }
+
+        public Task Invoke(Update update)
+        {
+            var info = update.GetInfoFromUpdate();
+
+            using (LogContext.PushProperty("UpdateType", info.UpdateType))
+            using (LogContext.PushProperty("MessageType", info.MessageType))
+            using (LogContext.PushProperty("From", info.FromName))
+            using (LogContext.PushProperty("Contents", info.Contents))
+            {
+                _logger.Information("{UpdateType} {MessageType} | {From}: {Contents}");
+            }
+
+            return _next.Invoke(update);
+        }
+    }
+}
