85110b87b6bd6917d9d68effa9470719e0991f2f
Askold Makaruk <askoldmakaruk@gmail.com>
2022-05-23 11:25:10 +0300

    v1.0.0-alpha-76

--- BotFramework/Localization/Texts.cs
+++ BotFramework/Localization/Texts.cs
@@ -4,14 +4,16 @@ namespace BotFramework.Localization;
 
 public class Texts
 {
+    private readonly IdentityUser         _user;
     private readonly TranslationContainer _container;
-    public readonly  string?              LocaleCode;
+    
+    public           string?              LocaleCode => _user?.LanguageCode;
 
     public string this[string key] => LocaleCode == null ? key : _container[LocaleCode][key];
 
     public Texts(IdentityUser user, TranslationContainer container)
     {
+        _user      = user;
         _container = container;
-        LocaleCode = user.LanguageCode;
     }
 }
